94ed5071175c3f5c2e5ccebc0c8c0c20b8ad4d2d
user-of-github-123 <156171457+user-of-github-123@users.noreply.github.com>
2024-03-07 15:32:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()  # variables from .env
-API_KEY = "6422329452:AFF_2jiXEE2Nqs9rAu7y2hZnM5CUh_B3bAk"
+API_KEY = os.environ.get("TELEGRAM_API_KEY")
 
 app = FastAPI()
 
@@ -21,7 +21,7 @@ async def webhook(req: Request):
     chat_id = bm['chat']['id']
     text = bm['text']
     # Send echo message
-    url = f"https://api.telegram.org/bot{6422329452:AFF_2jiXEE2Nqs9rAu7y2hZnM5CUh_B3bAk}/sendMessage"
+    url = f"https://api.telegram.org/bot{API_KEY}/sendMessage"
     payload = {"chat_id": chat_id,
         "text": text}
     response = requests.post(url, json=payload)
